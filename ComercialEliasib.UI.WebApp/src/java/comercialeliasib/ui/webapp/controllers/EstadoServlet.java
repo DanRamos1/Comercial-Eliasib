c19a06eabe19f43f5304f3a94796229914824cc6
Daniel Ramos <Dan13Ramos@outlook.com>
2022-08-31 08:04:57 -0600

    Actualizacion de UI Web y vistas

--- ComercialEliasib.UI.WebApp/src/java/comercialeliasib/ui/webapp/controllers/EstadoServlet.java
+++ ComercialEliasib.UI.WebApp/src/java/comercialeliasib/ui/webapp/controllers/EstadoServlet.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
+
 package comercialeliasib.ui.webapp.controllers;
 
 import java.io.IOException;
--- ComercialEliasib.UI.WebApp/src/java/comercialeliasib/ui/webapp/controllers/UsuarioServlet.java
+++ ComercialEliasib.UI.WebApp/src/java/comercialeliasib/ui/webapp/controllers/UsuarioServlet.java
@@ -4,84 +4,295 @@
  */
 package comercialeliasib.ui.webapp.controllers;
 
+import comercialeliasib.accesoadatos.RolDAL;
+import comercialeliasib.accesoadatos.UsuarioDAL;
+import comercialeliasib.entidadesdenegocio.Rol;
+import comercialeliasib.entidadesdenegocio.Usuario;
+import comercialeliasib.ui.webapp.utils.SessionUser;
+import comercialeliasib.ui.webapp.utils.Utilidad;
 import java.io.IOException;
-import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 
 /**
  *
  * @author Admin
  */
-@WebServlet(name = "UsuarioServlet", urlPatterns = {"/UsuarioServlet"})
+@WebServlet(name = "UsuarioServlet", urlPatterns = {"/Usuario"})
 public class UsuarioServlet extends HttpServlet {
+    private Usuario obtenerUsuario(HttpServletRequest request) {
+        String accion = Utilidad.GetParameter(request, "accion", "index");
+        Usuario usuario = new Usuario();
+        usuario.setNombre(Utilidad.GetParameter(request, "nombre", ""));
+        usuario.setApellido(Utilidad.GetParameter(request, "apellido", ""));
+        usuario.setLogin(Utilidad.GetParameter(request, "login", ""));
+        usuario.setIdRol(Integer.parseInt(Utilidad.GetParameter(request, "idRol", "0")));
+        usuario.setEstado(Byte.parseByte(Utilidad.GetParameter(request, "estado", "0")));
+        
+        if (accion.equals("index")) {
+            usuario.setTop_aux(Integer.parseInt(Utilidad.GetParameter(request, "top_aux", "10")));
+            usuario.setTop_aux(usuario.getTop_aux() == 0 ? Integer.MAX_VALUE : usuario.getTop_aux());
+        }
+        
+        if (accion.equals("login") || accion.equals("create") || accion.equals("cambiarpass")) {
+            usuario.setContrasenia(Utilidad.GetParameter(request, "contrasenia", ""));
+            usuario.setConfirmar_contrasenia(Utilidad.GetParameter(request, "confirmar_contrasenia", ""));
+            if (accion.equals("cambiarpass")) {
+                usuario.setId(Integer.parseInt(Utilidad.GetParameter(request, "id", "0")));
+            }
+        } else {
+            usuario.setId(Integer.parseInt(Utilidad.GetParameter(request, "id", "0")));
+        }
+        return usuario;
+    }
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet UsuarioServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet UsuarioServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+    private void doGetRequestIndex(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Usuario usuario = new Usuario();
+            usuario.setTop_aux(10);
+            ArrayList<Usuario> usuarios = UsuarioDAL.BuscarIncluirRol(usuario);
+            request.setAttribute("usuarios", usuarios);
+            request.setAttribute("top_aux", usuario.getTop_aux());
+            request.getRequestDispatcher("Views/Usuario/index.jsp").forward(request, response);
+        } catch (Exception ex) {
+            Utilidad.enviarError(ex.getMessage(), request, response);
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    private void doPostRequestIndex(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Usuario usuario = obtenerUsuario(request);
+            ArrayList<Usuario> usuarios = UsuarioDAL.BuscarIncluirRol(usuario);
+            request.setAttribute("usuarios", usuarios);
+            request.setAttribute("top_aux", usuario.getTop_aux());
+            request.getRequestDispatcher("Views/Usuario/index.jsp").forward(request, response);
+        } catch (Exception ex) {
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+
+    private void doGetRequestCreate(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("Views/Usuario/create.jsp").forward(request, response);
+    }
+
+    private void doPostRequestCreate(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Usuario usuario = obtenerUsuario(request);
+            int result = UsuarioDAL.Crear(usuario);
+            if (result != 0) {
+                request.setAttribute("accion", "index");
+                doGetRequestIndex(request, response);
+            } else {
+                Utilidad.enviarError("No se logro registrar un nuevo registro", request, response);
+            }
+        } catch (Exception ex) {
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+
+    }
+
+    private void requestObtenerPorId(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Usuario usuario = obtenerUsuario(request);
+            Usuario usuario_result = UsuarioDAL.ObtenerPorId(usuario);
+            if (usuario_result.getId() > 0) {
+                Rol rol = new Rol();
+                rol.setId(usuario_result.getIdRol());
+                usuario_result.setRol(RolDAL.ObtenerPorId(rol));
+                request.setAttribute("usuario", usuario_result);
+            } else {
+                Utilidad.enviarError("El Id:" + usuario_result.getId() + " no existe en la tabla de Usuario", request, response);
+            }
+        } catch (Exception ex) {
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+
+    private void doGetRequestEdit(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        requestObtenerPorId(request, response);
+        request.getRequestDispatcher("Views/Usuario/edit.jsp").forward(request, response);
+    }
+
+    private void doPostRequestEdit(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Usuario usuario = obtenerUsuario(request);
+            int result = UsuarioDAL.Modificar(usuario);
+            if (result != 0) {
+                request.setAttribute("accion", "index");
+                doGetRequestIndex(request, response);
+            } else {
+                Utilidad.enviarError("No se logro actualizar el registro", request, response);
+            }
+        } catch (Exception ex) {
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+
+    private void doGetRequestDetails(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        requestObtenerPorId(request, response);
+        request.getRequestDispatcher("Views/Usuario/details.jsp").forward(request, response);
+    }
+
+    private void doGetRequestDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        requestObtenerPorId(request, response);
+        request.getRequestDispatcher("Views/Usuario/delete.jsp").forward(request, response);
+    }
+
+    private void doPostRequestDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Usuario usuario = obtenerUsuario(request);
+            int result = UsuarioDAL.Eliminar(usuario);
+            if (result != 0) {
+                request.setAttribute("accion", "index");
+                doGetRequestIndex(request, response);
+            } else {
+                Utilidad.enviarError("No se logro eliminar el registro", request, response);
+            }
+        } catch (Exception ex) {
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+
+    private void doGetRequestLogin(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        SessionUser.cerrarSession(request);
+        request.getRequestDispatcher("Views/Usuario/login.jsp").forward(request, response);
+    }
+
+    private void doPostRequestLogin(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Usuario usuario = obtenerUsuario(request);
+            Usuario usuario_auth = UsuarioDAL.Login(usuario);
+            if (usuario_auth.getId() != 0 && usuario_auth.getLogin().equals(usuario.getLogin())) {
+                Rol rol = new Rol();
+                rol.setId(usuario_auth.getIdRol());
+                usuario_auth.setRol(RolDAL.ObtenerPorId(rol));
+                SessionUser.autenticarUser(request, usuario_auth);
+                response.sendRedirect("Home");
+            } else {
+                request.setAttribute("error", "Credenciales incorrectas");
+                request.setAttribute("accion", "login");
+                doGetRequestLogin(request, response);
+            }
+        } catch (Exception ex) {
+            request.setAttribute("error", ex.getMessage());
+        }
+    }
+
+    private void doGetRequestCambiarPassword(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Usuario usuario = new Usuario();
+            usuario.setLogin(SessionUser.getUser(request)); 
+            Usuario usuario_result = UsuarioDAL.Buscar(usuario).get(0);
+            if (usuario_result.getId() > 0) {
+                request.setAttribute("usuario", usuario_result);
+                request.getRequestDispatcher("Views/Usuario/cambiarPassword.jsp").forward(request, response);
+            } else {
+                Utilidad.enviarError("El Id:" + usuario_result.getId() + " no existe en la tabla de Usuario", request, response);
+            }
+        } catch (Exception ex) {
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+
+    private void doPostRequestCambiarPassword(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Usuario usuario = obtenerUsuario(request);
+            String passActual = Utilidad.GetParameter(request, "passwordActual", "");
+            int result = UsuarioDAL.CambiarPassword(usuario, passActual);
+            if (result != 0) {
+                response.sendRedirect("Usuario?accion=login");
+            } else {
+                Utilidad.enviarError("No se logro cambiar el password", request, response);
+            }
+        } catch (Exception ex) {
+            Utilidad.enviarError(ex.getMessage(), request, response);
+        }
+    }
+    // </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="Métodos para procesar las peticiones Get y Post">
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        String accion = Utilidad.GetParameter(request, "accion", "index");
+        if (accion.equals("login")) {
+            request.setAttribute("accion", accion);
+            doGetRequestLogin(request, response);
+        } else { 
+            SessionUser.authorize(request, response, () -> {
+                switch (accion) {
+                    case "index":
+                        request.setAttribute("accion", accion);
+                        doGetRequestIndex(request, response);
+                        break;
+                    case "create":
+                        request.setAttribute("accion", accion);
+                        doGetRequestCreate(request, response);
+                        break;
+                    case "edit":
+                        request.setAttribute("accion", accion);
+                        doGetRequestEdit(request, response);
+                        break;
+                    case "delete":
+                        request.setAttribute("accion", accion);
+                        doGetRequestDelete(request, response);
+                        break;
+                    case "details":
+                        request.setAttribute("accion", accion);
+                        doGetRequestDetails(request, response);
+                        break;
+                    case "cambiarpass":
+                        request.setAttribute("accion", accion);
+                        doGetRequestCambiarPassword(request, response);
+                        break;
+                    default:
+                        request.setAttribute("accion", accion);
+                        doGetRequestIndex(request, response);
+                }
+            });
+        }
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        String accion = Utilidad.GetParameter(request, "accion", "index");
+        if (accion.equals("login")) {
+            request.setAttribute("accion", accion);
+            doPostRequestLogin(request, response);
+        } else { 
+            SessionUser.authorize(request, response, () -> {
+                switch (accion) {
+                    case "index":
+                        request.setAttribute("accion", accion);
+                        doPostRequestIndex(request, response);
+                        break;
+                    case "create":
+                        request.setAttribute("accion", accion);
+                        doPostRequestCreate(request, response);
+                        break;
+                    case "edit":
+                        request.setAttribute("accion", accion);
+                        doPostRequestEdit(request, response);
+                        break;
+                    case "delete":
+                        request.setAttribute("accion", accion);
+                        doPostRequestDelete(request, response);
+                        break;
+                    case "cambiarpass":
+                        request.setAttribute("accion", accion);
+                        doPostRequestCambiarPassword(request, response);
+                        break;
+                    default:
+                        request.setAttribute("accion", accion);
+                        doGetRequestIndex(request, response);
+                }
+            });
+        }
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
